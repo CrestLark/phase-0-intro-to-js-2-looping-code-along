da5a2f0f5df60035884259a5dd59ae9d68cb7520
crestlark <stephanie.lewis.art@gmail.com>
2021-06-30 12:22:01 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names = (["Lisa", "Kaitlin", "Jan"], "surprise");
+
+function writeCards(names) {
+   let messages=[];
+   for( let i= 0; i < names.length; i++) {
+       messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+   }
+   return messages;
+}
+
+
+function countDown(integers) {
+    let i= 10 
+    while (i >= 0){
+        console.log(i)
+        i--;
+    }
+    debugger;
+    return integers;
+}
